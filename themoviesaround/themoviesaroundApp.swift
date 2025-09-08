4773aa0e6b4a1b4e3b122e0f08ea5d80c6a379a5
Santosh Lakhani <lakhanisantosh@gmail.com>
2025-09-08 13:37:10 +0530

    Initial Commit

---
+++ themoviesaround/themoviesaroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  themoviesaroundApp.swift
+//  themoviesaround
+//
+//  Created by Santosh Lakhani on 08/09/25.
+//
+
+import SwiftUI
+
+@main
+struct themoviesaroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ themoviesaroundTests/themoviesaroundTests.swift
@@ -0,0 +1,16 @@
+//
+//  themoviesaroundTests.swift
+//  themoviesaroundTests
+//
+//  Created by Santosh Lakhani on 08/09/25.
+//
+
+import Testing
+
+struct themoviesaroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
